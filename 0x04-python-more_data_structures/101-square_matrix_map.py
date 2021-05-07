6079afc0ee08e40ee0db9da5e289c3af61120484
anamariaroman <2979@holbertonschool.com>
2021-05-06 23:51:45 -0500

    task update

---
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+
+def square_matrix_map(matrix=[]):
+    return list(map((lambda line: list(map((lambda a: a * a), line))), matrix))
---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    return {the_key: new * 2 for the_key, new in a_dictionary.items()}
