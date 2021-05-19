8fb5a43818d44c893aea4f7d73f95ccc076edabc
anamariaroman <2979@holbertonschool.com>
2021-05-18 23:35:52 -0500

    update

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Definition square class"""
+
+
+class Square:
+    """class"""
+
+    def __init__(self, size=0):
+        """Constructor to initialize the attributes"""
+
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+            return
+        if size < 0:
+            raise ValueError("size must be >= 0")
+            return
+
+        self.__size = size
+
+    def area(self):
+        """Return the square size"""
+        return self.__size ** 2
