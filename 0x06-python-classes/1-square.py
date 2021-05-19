416525b3954cdf864187d3091abe5d480d80bb7c
anamariaroman <2979@holbertonschool.com>
2021-05-18 23:28:57 -0500

    tasks

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Definition square class"""
+
+
+class Square:
+    """class"""
+
+    def __init__(self, size):
+        self.__size = size
---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Definition square class"""
+
+
+class Square:
+    """class"""
+
+    def __init__(self, size=0):
+        """Constructor to initialize the attributes"""
+
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+            return
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+            return
+
+        self.__size = size
