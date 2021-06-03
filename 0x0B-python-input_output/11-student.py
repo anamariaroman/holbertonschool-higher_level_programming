e14617f6661762fa477ae47e1805c270b2b042e8
anamariaroman <2979@holbertonschool.com>
2021-06-03 00:10:08 -0500

    fix number 1000 xd

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -3,13 +3,25 @@
 
 
 class Student:
-    """ List of a student"""
+    """ Students information """
     def __init__(self, first_name, last_name, age):
-        """Initializes the student"""
+        """ Initializate """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
-        """returns the dictionary"""
-        return self.__dict__
+    def to_json(self, attrs=None):
+        """The directory"""
+        if attrs is None:
+            return self.__dict__
+        else:
+            new_aux = {}
+            for i in attrs:
+                if i in self.__dict__:
+                    new_aux[i] = self.__dict__[i]
+            return new_aux
+
+    def reload_from_json(self, json):
+        """ replaces the attributes of the class """
+        for a in json:
+            self.__dict__[a] = json[a]
