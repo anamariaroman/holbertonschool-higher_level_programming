bfdc42ca80ef7d19dedc548b9862b803573f8a0a
anamariaroman <2979@holbertonschool.com>
2021-07-13 23:32:30 -0500

    commits changes

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,5 @@
+-- Import the database dump of hbtn_0d_tvshows: download (same as 10-genre_id_by_show.sql)
+-- Lists all shows contained in the database hbtn_0d_tvshows
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
