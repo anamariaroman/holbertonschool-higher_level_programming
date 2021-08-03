637e03a5d4691761d75e7d5544bf5eba7e7d677a
anamariaroman <2979@holbertonschool.com>
2021-08-02 23:35:21 -0500

    task 5 again fix

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,6 @@
-#!/usr/bin/node
-if (parseInt(process.argv[2])) {
-    console.log('My number: ' + parseInt(process.argv[2]));
-  } else {
-    console.log('Not a number');
-  }
-  
+const argument = process.argv[2];
+if (isNaN(argument)) {
+  console.log('Not a number');
+} else {
+  console.log('My number: ' + argument);
+}
