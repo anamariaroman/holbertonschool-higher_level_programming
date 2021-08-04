e8808594acfa1680b4c3472d853cea369dec9c0b
anamariaroman <2979@holbertonschool.com>
2021-08-03 21:40:52 -0500

    task 8

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+exports.esrever = function (list) {
+    const temporalList = [];
+    for (const i of list) {
+      temporalList.unshift(i);
+    }
+    return temporalList;
+  };
