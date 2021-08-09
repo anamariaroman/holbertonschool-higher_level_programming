d6a6a5c215e51e7bb1121df63d37100b12e9af5d
anamariaroman <2979@holbertonschool.com>
2021-08-08 21:46:16 -0500

    update changes task 0 and 1

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -9,7 +9,7 @@ from sys import argv
 if __name__ == "__main__":
     # Data connection
     database = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                         passwd=argv[2], db=argv[3], charset="utf8")
+                                    passwd=argv[2], db=argv[3], charset="utf8")
     cursor = database.cursor()
     cursor.execute("SELECT * FROM `states` ORDER BY `states`.`id` ASC")
     All = cursor.fetchall()
