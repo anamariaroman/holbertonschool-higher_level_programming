cc427bd4881be1f897f1986e1dd1fb6e4cf05cf9
anamariaroman <2979@holbertonschool.com>
2021-08-20 18:05:10 -0500

    task 2 fix

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -10,6 +10,5 @@ from sys import argv
 if __name__ == "__main__":
     values = {'email': argv[2]}
     data = urllib.parse.urlencode(values).encode('utf-8')
-    with urlopen(Request(argv[1], data)) as response:
-        print(response.read().decode('utf-8'))
-
+    with urlopen(Request(argv[1], data)) as resp:
+        print(resp.read().decode('utf-8'))
